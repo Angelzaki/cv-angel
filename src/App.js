9ef9d0a2163b89cd08e356d54956c2deb65f79bb
Angel David (Zaki) <140128799+Angelzaki@users.noreply.github.com>
2024-12-05 02:18:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         <section id="perfil" className="mb-8">
           <h2 className="text-3xl font-semibold">Perfil</h2>
           <p className="mt-2 text-justify">
-            Estudiante de Ingeniería de Sistemas apasionado por el desarrollo de software, con habilidades en metodologías ágiles y herramientas tecnológicas. Experiencia en proyectos que involucran desarrollo de soluciones innovadoras, optimización de procesos y trabajo en equipo. Busco oportunidades para aplicar mis habilidades en un entorno dinámico y colaborativo.
+            Estudiante de Ingeniería de Sistemas con experiencia en aseguramiento de calidad de software y desarrollo de proyectos innovadores. Apasionado por garantizar productos de software de alta calidad, utilizando metodologías ágiles como Scrum, ITIL y TOGAF, así como herramientas de automatización como Cypress, Selenium y Postman. Competente en el uso de tecnologías de análisis y visualización como Power BI y en lenguajes de programación como Python, JavaScript y Java. Con un enfoque colaborativo y orientado a resultados, busco aportar al desarrollo de soluciones tecnológicas que impacten positivamente en organizaciones dinámicas y en constante evolución.
           </p>
         </section>
         <section id="educacion" className="mb-8">
@@ -47,11 +47,24 @@ function App() {
           <p className="text-justify">Ingeniería de sistemas e informática, X ciclo</p>
           <p className="text-justify">Quinto Superior</p>
         </section>
+        <section id="experiencia" className="mb-8">
+          <h2 className="text-3xl font-semibold">Experiencia Laboral</h2>
+          <div className="mt-4">
+            <h3 className="text-2xl font-semibold">Practicante de Arquitectura y Desarrollo en TI – Enotria SA</h3>
+            <p>Septiembre 2024 – Actualidad</p>
+            <ul className="list-disc list-inside ml-4 mt-2 text-justify">
+              <li>Desarrollo y mantenimiento de software y flujos de trabajo con la plataforma PrintNet, contribuyendo a la mejora continua de los sistemas tecnológicos de la empresa.</li>
+              <li>Realización de pruebas de control de calidad y estrés de software, garantizando la eficiencia y el rendimiento de las aplicaciones.</li>
+              <li>Participación en tareas afines asignadas, asegurando el cumplimiento de objetivos y la adaptación a las necesidades del equipo.</li>
+              <li>Monitoreo continuo de dashboards ya implementados, asegurando su correcto funcionamiento y rápida resolución de incidencias críticas los cuales hago uso de herramientas como python, .NET y AWS.</li>
+            </ul>
+          </div>
+        </section>
         <section id="actividades" className="mb-8">
           <h2 className="text-3xl font-semibold">Actividades Extracurriculares</h2>
           <div className="mt-4">
             <h3 className="text-2xl font-semibold">Scrum Master / Control de Calidad - No Country</h3>
-            <p>Junio 2024 – Agosto 2024</p>
+            <p>Junio 2024 – Actualidad</p>
             <ul className="list-disc list-inside ml-4 mt-2 text-justify">
               <li>Facilité reuniones diarias y retrospectivas, asegurando la adherencia a los principios de Scrum y optimizando la gestión del tiempo y los recursos.</li>
               <li>Coordiné con desarrolladores y diseñadores para resolver problemas y alinearse con los objetivos del proyecto.</li>
@@ -61,10 +74,10 @@ function App() {
           </div>
           <div className="mt-8">
             <h3 className="text-2xl font-semibold">Proyecto Personal: Chatbot de IA para Diagnóstico de Dermatitis</h3>
-            <p>Julio 2024 – Presente</p>
+            <p>Julio 2024 – Diciembre 2024</p>
             <ul className="list-disc list-inside ml-4 mt-2 text-justify">
-              <li>Desarrollando un chatbot utilizando inteligencia artificial para la identificación y clasificación de tipos de dermatitis, centrado en mejorar la precisión diagnóstica en el sector salud.</li>
-              <li>Implemento técnicas de procesamiento del lenguaje natural (PLN) para desarrollar el chatbot.</li>
+              <li>Desarrolle un chatbot utilizando inteligencia artificial para la identificación y clasificación de tipos de dermatitis, centrado en mejorar la precisión diagnóstica en el sector salud.</li>
+              <li>Implemente técnicas de procesamiento del lenguaje natural (PLN) para desarrollar el chatbot.</li>
               <li>Analicé datos de interacción para evaluar la precisión del diagnóstico y la efectividad en la información proporcionada al usuario.</li>
             </ul>
           </div>
@@ -72,18 +85,19 @@ function App() {
         <section id="habilidades" className="mb-8">
           <h2 className="text-3xl font-semibold">Habilidades Técnicas</h2>
           <ul className="list-disc list-inside ml-4 mt-2 text-justify">
-            <li><strong>Lenguajes de Programación:</strong> Java, JavaScript, Python, HTML, CSS.</li>
-            <li><strong>Frameworks y Librerías:</strong> React, Angular, React Native.</li>
-            <li><strong>Bases de Datos:</strong> SQL Server, Oracle, MySQL, Firebase.</li>
+            <li><strong>Lenguajes de Programación:</strong> Java, JavaScript, Python, HTML, CSS, C#.</li>
+            <li><strong>Frameworks y Librerías:</strong> React, Angular, React Native, .NET, Node.js.</li>
+            <li><strong>Bases de Datos:</strong> SQL Server, Oracle, MySQL, Firebase, MongoDB.</li>
             <li><strong>Herramientas de Prueba y Automatización:</strong> Cypress, Selenium, Postman, JMETER.</li>
             <li><strong>Metodologías y Marcos de Trabajo:</strong> Scrum, ITIL, TOGAF, Lean Six Sigma.</li>
-            <li><strong>Otros:</strong> Power BI, Visual Studio, Power Apps, Power Automate, Office 365.</li>
+            <li><strong>Otros:</strong> Power BI, Visual Studio, Power Apps, Power Automate, Office 365, IA, ML, DL.</li>
           </ul>
         </section>
         <section id="certificados" className="mb-8">
           <h2 className="text-3xl font-semibold">Certificados</h2>
           <ul className="list-disc list-inside ml-4 mt-2 text-justify">
             <li>Manejo de Inglés a Nivel Avanzado, ICPNA, 2021.</li>
+            <li>ISO/IEC 27001:2022 Foundation (I27001F), CertiProf, 2024.</li>
             <li>Scrum Master Professional Certificate - SMPC®, CertiProf, 2024.</li>
             <li>Fundamentos de la Dirección de Proyectos Predictive, PMI, 2024.</li>
             <li>QA desde cero: Principio, habilidades y el futuro con IA, Desafio Latam, 2024.</li>
